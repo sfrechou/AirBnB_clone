762aa44e02efb651b747ff27a3ee8da33d3c26e2
Soledad Frechou <solefrechou@hotmail.com>
2021-02-17 15:11:48 +0000

    Add

--- models/base_model.py
+++ models/base_model.py
@@ -4,51 +4,42 @@ import uuid
 import datetime
 import csv
 from models import storage
-
-
+ 
+ 
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
-
+ 
     def __init__(self, *args, **kwargs):
         """Initializes object"""
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
-                    formatx = "%Y-%m-%dT%H:%M:%S.%f"
                     if key == "created_at":
-                        value = datetime.datetime.strptime(value, formatx)
+                        value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     if key == "updated_at":
-                        value = datetime.datetime.strptime(value, formatx)
+                        value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
         else:
-            formatx = "%Y-%m-%dT%H:%M:%S.%f"
             self.id = str(uuid.uuid4())
-            x = datetime.datetime.now()
-            self.created_at = x.strftime(formatx)
-            self.updated_at = x.strftime(formatx)
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
             storage.new(self)
-
+ 
     def __str__(self):
         """Returns string representation"""
         return ("[{}] ({}) {}".format(self.__class__.__name__,
                                       self.id, self.__dict__))
-
+ 
     def save(self):
         """Updates the public instance attr with current datetime"""
-        formatx = "%Y-%m-%dT%H:%M:%S.%f"
-        x = datetime.datetime.now()
-        self.updated_at = x.strftime(formatx)
+        self.updated_at = datetime.datetime.now()
         storage.save()
-
-
+ 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__"""
-        dict_returned = {}
-        dict_returned["__class__"] = self.__class__.__name__
-        for key, value in self.__dict__.items():
-            if type(value) == datetime.datetime:
-                dict_returned[key] = value.isoformat()
-            else:
-                dict_returned[key] = value
-        return (dict_returned)
+        self.__dict__["__class__"] = self.__class__.__name__
+        self.__dict__["created_at"] = self.created_at.isoformat()
+        self.__dict__["updated_at"] = self.updated_at.isoformat()
+        return self.__dict__
+ 
 
