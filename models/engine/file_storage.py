a30ea4f569399cda3faac2d043f850604665f104
Soledad Frechou <solefrechou@hotmail.com>
2021-02-18 19:51:24 +0000

    Add

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,13 +3,13 @@
 import json
 import os
 import datetime
-
-
-def myconverter(o):
-    """converter from datetime to str"""
-    if isinstance(o, datetime.datetime):
-        return o.__str__()
-
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """
@@ -27,31 +27,45 @@ class FileStorage:
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
         # obj.__dict__["updated_at"] = str(obj.__dict__["created_at"])
-        # obj.__dict__["created_at"] = str(obj.__dict__["created_at"])
+        # obj.__dict__["created_at"] = str(obj.__dict__["created_at"]
         self.__objects[str(obj.__class__.__name__) +
                        "." + obj.id] = obj.__dict__
 
     def save(self):
-        """serializes __objects to a JSON path from __file_path"""
-        if self.__objects:
-            for key25, value25 in self.__objects.items():
-                newdict = value25.copy()
-                for key225, value225 in newdict.items():
-                    if not value225:
-                        del value25[key225]
-                self.__objects[key25] = value25
-        new_dict = {}
+        """Serializes __objects to the JSON file"""
+        new_dict = self.__objects
+        for key, value in new_dict.items():
+            class_name = key.split(".")
+            if type(value) == str:
+                list_val = value.split()
+                del list_val[:2]
+                new_str = ""
+                for ele in list_val:
+                    new_str += ele
+            else:
+                value["__class__"] = class_name[0]
+                new_dict[key] = str(value)
+
+        with open(FileStorage.__file_path, "w") as my_file:
+            json.dump(new_dict, my_file)
+        
         for key, value in self.__objects.items():
-            new_dict[key] = value
-        with open(FileStorage.__file_path, 'w') as filee:
-            json.dump(new_dict, filee, default=myconverter)
+            if type(value) == dict:
+                del value["__class__"]
+        
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
         if os.path.isfile(self.__file_path):
             with open(self.__file_path, 'r') as my_file:
                 one_obj_dictionary = json.load(my_file)
-                for key, value in one_obj_dictionary.items():
-                    self.__objects[key] = value
+                for i, j in one_obj_dictionary.items():
+                    k = eval(j)
+                    del k["__class__"]
+                    j = str(k)
+                    new_id = i.split(".")
+                    new = "[" + new_id[0] + "] (" + new_id[1] + ")"
+                    # self.__objects[i] = new + " " + j
+                    self.new(eval(new_id[0])(**k))
         else:
             return
