63f94eec841b5ffe76081c24c9264a40934182d0
Soledad Frechou <solefrechou@hotmail.com>
2021-02-18 21:02:09 +0000

    ADD TEST CITY.PY

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+""" Tests for City class """
+import unittest
+import datetime
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.city import City
+import models
+
+
+class TestingCity(unittest.TestCase):
+    """ City class - tests """
+    def test_City1(self):
+        """ Test of the City class """
+        my_City1 = City()
+
+    def test_City2(self):
+        """ Test of the City class """
+        my_City2 = City(1)
+        my_City3 = City("hola")
+        my_City4 = City([1, 2, 3])
+        my_City5 = City({"hola": "chau"})
+
+    def test_City3(self):
+        """ Test of the City class """
+        self.assertEqual(type(City), type)
+
+    def test_City4(self):
+        """ Test of the City class """
+        my_City6 = City()
+        self.assertEqual(isinstance(my_City6, City), True)
+
+    def test_City5(self):
+        """ Test of the City class """
+        my_City7 = City()
+        self.assertEqual(issubclass(City, BaseModel), True)
+
+    def test_City6(self):
+        """ Test of the City class """
+        self.assertEqual(issubclass(City, FileStorage), False)
+
+    def test_City7(self):
+        """ Test of the City class """
+        my_City8 = City()
+        my_City8.name = "Denver"
+        self.assertEqual(type(my_City8.state_id), str)
+        self.assertEqual(type(my_City8.name), str)
+
+    def test_City8(self):
+        """ Test of the City class """
+        my_City9 = City()
+        my_City9.state_id = "Borussia"
+        my_City9.name = "Dortmund"
+        self.assertTrue("state_id" in my_City9.__dict__)
+        self.assertTrue("name" in my_City9.__dict__)
+
+    def test_City9(self):
+        """ Test of the City class """
+        my_City10 = City()
+        self.assertFalse("first" in my_City10.__dict__)
+
+    def test_City11N(self):
+        self.assertEqual(City, type(City()))
+        self.assertEqual(str, type(City().id))
+
+    def test_City12N(self):
+        my_city11 = City()
+        my_city12 = City()
+        self.assertNotEqual(my_city11.id, my_city12.id)
+
+if __name__ == "__main__":
+    unittest.main()
