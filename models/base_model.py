a55604a5ed43358c6f34411e7db0b73c8e386f3f
JulianArbini97 <1890@holbertonschool.com>
2021-02-19 01:15:01 +0000

    file

--- models/base_model.py
+++ models/base_model.py
@@ -44,4 +44,4 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         new_dict["updated_at"] = self.updated_at.isoformat()
         new_dict["created_at"] = self.created_at.isoformat()
-        return inst_dict
+        return new_dict
